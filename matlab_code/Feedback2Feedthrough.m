90a6c7b83b736ac3ee41ad3dcfff6ef0ebf3e2a5
Diasaur <33789557+Diasaur@users.noreply.github.com>
2025-11-08 18:55:51 +1100

    Delete old matlab_code directory

--- matlab_code/Feedback2Feedthrough.m
+++
@@ -1,8 +0,0 @@
-function F = Feedback2Feedthrough(FTilde,D)
-% Recalculate feedback gain for the case with a feedthrough matrx
-
-temp = size(FTilde);
-m = temp(1);
-F = (eye(m)+FTilde*D)\FTilde;
-
-end
